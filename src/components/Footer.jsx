8df11fcdd4bfd4304cf93b59aa57b32c9dc75205
desireemezzotero <desireemezzotero1@gmail.com>
2025-01-14 16:06:46 +0100

    [add] footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,12 @@ import merchandise from '../assets/img/buy-comics-merchandise.png';
 import shop from '../assets/img/buy-comics-shop-locator.png';
 import subscriptions from '../assets/img/buy-comics-subscriptions.png';
 import pay from '../assets/img/buy-dc-power-visa.svg';
-import imgBg from '../assets/img/dc-logo-bg.png'
-
+import imgBg from '../assets/img/dc-logo-bg.png';
+import facebook from '../assets/img/footer-facebook.png';
+import twitter from '../assets/img/footer-twitter.png';
+import youTube from '../assets/img/footer-youtube.png';
+import pinterest from '../assets/img/footer-pinterest.png';
+import periscope from '../assets/img/footer-periscope.png';
 
 const Footer = () => {
   return (
@@ -44,25 +48,83 @@ const Footer = () => {
         <div>
           <img className='img-bg' src={imgBg} alt="" />
         </div>
-        <div className='container'>
-        <div className='text p-4'>
-          <ul>
-            <li>
-             <h6>
+        <div className='container d-flex'>
+          <div className='text-footer p-4'>
+            <h6>
               DC COMICS
-             </h6>
-            </li>
-            <li>
-              <a href="#">
-              Characters
-              </a>
-            </li>
-          </ul>
-        </div>
-        </div>
-    
+            </h6>
+           <ul>
+             <li> <a href="#"> Characters </a> </li>
+             <li> <a href="#"> Comics </a> </li>
+             <li> <a href="#"> Movies </a> </li>
+             <li> <a href="#"> TV </a> </li>
+             <li> <a href="#"> Games </a> </li>
+             <li> <a href="#"> Videos </a> </li>
+             <li> <a href="#"> News </a> </li>
+           </ul>
 
+           <h6>
+             SHOP
+           </h6>
+           <ul>
+            <li> <a href="#"> Shop DC </a> </li>
+            <li> <a href="#"> Shop DC Collectibles </a> </li>
+           </ul>
+         </div>
+
+         <div className='text-footer p-4'>
+            <h6>
+              DC
+            </h6>
+           <ul>
+             <li> <a href="#"> Terms Of Use </a> </li>
+             <li> <a href="#"> Privacy policy (New) </a> </li>
+             <li> <a href="#"> Ad Choices </a> </li>
+             <li> <a href="#"> Advertising </a> </li>
+             <li> <a href="#"> Job </a> </li>
+             <li> <a href="#"> Subscriptions </a> </li>
+             <li> <a href="#"> Talent Workshops </a> </li>
+             <li> <a href="#"> CPSC Certificates </a> </li>
+             <li> <a href="#"> Ratings </a> </li>
+             <li> <a href="#"> Shop Help </a> </li>
+             <li> <a href="#"> Contact Us </a> </li>
+           </ul>
+         </div>
+
+         <div className='text-footer p-4'>
+            <h6>
+              SITES
+            </h6>
+           <ul>
+             <li> <a href="#"> DC </a> </li>
+             <li> <a href="#"> MAD Magazine </a> </li>
+             <li> <a href="#"> DC Kids </a> </li>
+             <li> <a href="#"> DC Universe </a> </li>
+             <li> <a href="#"> DC Power Visa </a> </li>
+           </ul>
+         </div>
+        </div>
       </div>
+
+      <section className='container-black'>
+        <div className='container justify-content-between d-flex'>
+          <div className='btn'>
+            <button type="button" class="btn btn-outline-primary button">SIGN-UP NOW!</button>
+          </div>
+
+          <div className='social d-flex'>
+            <h6 className= 'follow'>FOLLOW US</h6>
+            <ul> 
+              <li> <a href="#"><img src={facebook} alt="facebook" /></a> </li>
+              <li> <a href="#"><img src={twitter} alt="twittwe" /></a> </li>
+              <li> <a href="#"><img src={youTube} alt="youtube" /></a> </li>
+              <li> <a href="#"><img src={pinterest} alt="pinterest" /></a> </li>
+              <li> <a href="#"><img src={periscope} alt="periscope" /></a> </li>
+            </ul>
+          </div>
+
+        </div>
+      </section>
     </footer>
   )
 }
